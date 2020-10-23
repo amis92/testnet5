24eaa8b4b00eac368d8c75c3c396594ac75fa316
Amadeusz Sadowski <amis.tau.92@gmail.com>
2020-10-23 15:01:23 +0200

    initial repro

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine(System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription);
+        X();
+    }
+
+    static void X() => new MyListNode();
+}
+
+class Node { }
+
+struct NodeList<T> where T : Node
+{
+    public static implicit operator NodeList<Node>(NodeList<T> nodeList) => new NodeList<Node>();
+}
+
+abstract class ListNode<T> where T : Node
+{
+    public abstract ListNode<T> WithNodes(NodeList<T> nodes);
+}
+
+class MyNode : Node { }
+
+class MyListNode : ListNode<MyNode>
+{
+    public override MyListNode WithNodes(NodeList<MyNode> nodes) => new MyListNode();
+}
